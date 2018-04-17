92483c2a450edb0545485595fbf9faca38a5ae94
Hector Moreno <hectormov@gmail.com>
2018-04-17 01:50:32 -0500

    Feature: Flipping, comparing and moves
    Added flipping and updated CSS accordingly
    Added comparisson of cards
    Added Move counter
    Added header

--- js/memory.js
+++ js/memory.js
@@ -1,36 +1,65 @@
 
 
 const board = document.querySelector('#gameBoard');
+
+
 let level = 'easy';
+let card1 = '';
+let moves = 0;
 
 function buildBoard(level) {
   let size = 4;
   let htmlTextToAdd = '';
   board.innerHTML = '';
-  for (let r = 1; r <= size*size; r++){
-    htmlTextToAdd += '<div class="cardContainer"><div class="card"><div class="front">front</div><div class="back">back</div></div></div>';
+  for (let i = 1; i <= size*size; i++){
+    htmlTextToAdd += '<div class="cardContainer"><div class="card"><div class="front">front</div><div class="back">back'+i+'</div></div></div>';
   }
   board.insertAdjacentHTML('afterbegin',htmlTextToAdd);
 }
-// function buildBoard(level) {
-//   let size = 4;
-//   let htmlTextToAdd = '';
-//   board.innerHTML = '';
-//   for (let r = 1; r <= size; r++){
-//     htmlTextToAdd += '<tr>'
-//     for (let c = 1; c <= size; c++){
-//       htmlTextToAdd += '<td class="card"></td>'
-//     }
-//     htmlTextToAdd += '</tr>'
-//   }
-//   board.insertAdjacentHTML('afterbegin',htmlTextToAdd);
-// }
 
 function flipCard(event) {
   let card = event.target;
-    card.firstChild.classList.toggle("flip");
+  if (card.id !== 'gameBoard' && !card.classList.contains("cardContainer")
+  && !card.classList.contains("card") && card1 !== card) {
+    console.log(card);
+    card.parentElement.classList.toggle("flip");
+    if (card.classList.contains('back')) {
+      let pickedCard = card.previousElementSibling;
+      setTimeout(function (){
+        compareCards(pickedCard);
+      },500)
+    }
+  }
+}
 
+function compareCards(pickedCard) {
+  console.log(pickedCard);
+  if (card1 === '') {
+    card1 = pickedCard;
+    return;
+  }
+  else if (card1.innerText == pickedCard.innerText) {
+    pickedCard.parentElement.classList.toggle("flip");
+    card1.parentElement.classList.toggle("flip");
+    card1 = '';
+    increaseMoves();
+    return;
+  }
+  matchFound(pickedCard);
+  increaseMoves();
+  card1 = '';
+}
+
+function matchFound(pickedCard) {
+  card1.parentElement.classList.add("found");
+  pickedCard.parentElement.classList.add("found");
+}
+
+function increaseMoves() {
+  const moveCounter = document.querySelector('.moves');
+  moveCounter.innerText = parseInt(moveCounter.innerText) + 1;
+  return moves;
 }
 
 buildBoard(level);
-board.addEventListener('click',flipCard);
+board.addEventListener('click',flipCard,true);
