098387743b4a4cde2050d5007a69928f1ad908cd
Hector Moreno <hectormov@gmail.com>
2018-04-18 00:06:38 -0500

    Feature: Timer, Rating, Clear, move counter, winner and comments, initial modal
    Implemented timer
    Implemented Rating system
    Added button to clear/restart game
    Fixed bug on move counter
    Determines winner
    Started working on Modal

--- js/memory.js
+++ js/memory.js
@@ -1,12 +1,24 @@
 
 
 const board = document.querySelector('#gameBoard');
-
+const headerTimer = document.querySelector('.time');
+const resetButton = document.querySelector('button');
+const moveCounter = document.querySelector('.moves');
+const starsSpan = document.querySelector('.stars');
 
 let level = 'easy';
 let card1 = '';
 let moves = 0;
+let foundPairTarget = 8;
+let foundPairCount = 0;
+let timer = null;
+let count = 0;
 
+/*
+Creates the board with a certain amount of cards (Default 4x4)
+TODO: I want to introduce different difficulty levels with more cards so I thought to create this dynamically
+and not set the deck to fixed.
+*/
 function buildBoard(level) {
   let size = 4;
   let htmlTextToAdd = '';
@@ -17,11 +29,14 @@ function buildBoard(level) {
   board.insertAdjacentHTML('afterbegin',htmlTextToAdd);
 }
 
+/*
+As long as only the front or back are clicked, flippes the card, additionally if the back is clicked
+we call compareCards to check if the front matches
+*/
 function flipCard(event) {
   let card = event.target;
   if (card.id !== 'gameBoard' && !card.classList.contains("cardContainer")
   && !card.classList.contains("card") && card1 !== card) {
-    console.log(card);
     card.parentElement.classList.toggle("flip");
     if (card.classList.contains('back')) {
       let pickedCard = card.previousElementSibling;
@@ -32,10 +47,20 @@ function flipCard(event) {
   }
 }
 
+/*
+Receives the clicked card, if it is the very first card it startes the timer and if it is the
+first guess for a pair it stops the function.
+If it is the second card of the pair, it compares the content, resets the check for first card of the pair
+and increases the move count.
+If the cards are different both cards are flipped back
+If the cards are the same matchfound function is called.
+*/
 function compareCards(pickedCard) {
-  console.log(pickedCard);
   if (card1 === '') {
     card1 = pickedCard;
+    if (moves === 0) {
+      startTimer();
+    }
     return;
   }
   else if (card1.innerText !== pickedCard.innerText) {
@@ -47,19 +72,78 @@ function compareCards(pickedCard) {
   }
   matchFound(pickedCard);
   increaseMoves();
-  card1 = '';
+  if (foundPairCount === foundPairTarget) {
+    winner();
+  }
+  else {
+    card1 = '';
+  }
 }
 
+//Marks the cards are found so they are not clickable again and stay flipped, increases the found pairs count
 function matchFound(pickedCard) {
   card1.parentElement.classList.add("found");
   pickedCard.parentElement.classList.add("found");
+  foundPairCount++;
 }
 
 function increaseMoves() {
-  const moveCounter = document.querySelector('.moves');
-  moveCounter.innerText = parseInt(moveCounter.innerText) + 1;
-  return moves;
+  moves++;
+  moveCounter.innerText = moves;
+  checkRating();
+}
+
+function startTimer() {
+  timer = setInterval(function(){
+    increaseTime();
+  },1000);
+}
+
+function increaseTime(){
+  count++;
+  time = new Date(count * 1000).toISOString().substr(11, 8);
+  headerTimer.innerText = time;
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
+
+//Change all variables used back to default
+function reset() {
+  card1 = '';
+  moves = 0;
+  stopTimer();
+  headerTimer.innerText = "00:00:00";
+  timer = null;
+  count = 0;
+  moveCounter.innerText = "0";
+  foundPairCount = 0;
+  buildBoard(level);
+  starsSpan.innerText = "***"
+}
+
+function winner() {
+  stopTimer();
+  console.log("WE HAVE A WINNEEEEEEERRRRR!!!!")
+}
+
+//Updates the rating according to certain moves, must change to parameters once difficulty is implemented
+function checkRating() {
+  switch (moves) {
+    case 9:
+      starsSpan.innerText = "**"
+      break;
+    case 12:
+      starsSpan.innerText = "*"
+      break;
+    default:
+      break;
+  }
 }
 
 buildBoard(level);
-board.addEventListener('click',flipCard,true);
+
+// Events
+board.addEventListener('click',flipCard);
+resetButton.addEventListener('click',reset);
